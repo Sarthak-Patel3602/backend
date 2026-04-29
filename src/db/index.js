d99428fcd1e462a9b3552381d63be52c4dfa3911
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2026-04-29 17:44:22 +0530

    feat: update environment configuration and add cookie-parser dependency

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-dotenv.config();
+
 
 const connectDB = async () => {
     try {
-        console.log("hello");
-        const connectionInstance = await mongoose.connect('mongodb+srv://Sarthak:Sarthak2104@cluster0.bmcnto3.mongodb.net/videotube');
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB Connection Error", error);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+dotenv.config();
 
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log('MongoDB connection failed !!!', err);
+    })
 
 
 
